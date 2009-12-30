8c049795a8505880579afb61582186b1156b945c
Toby Boudreaux <toby@stickler.home>
2009-12-29 21:29:00 -0500

    Removed the haml/sass stuff from glue, moved it to helpers.

--- app/helpers/main.rb
+++ app/helpers/main.rb
@@ -1,6 +1,19 @@
 class Main
   helpers do
+    class Monk::Glue < Sinatra::Base
+      helpers do
+        # TODO Add documentation.
+        def haml(template, options = {}, locals = {})
+          options[:escape_html] = true unless options.include?(:escape_html)
+          super(template, options, locals)
+        end
 
+        # TODO Add documentation.
+        def partial(template, locals = {})
+          haml(template, {:layout => false}, locals)
+        end
+      end
+    end
     # Your helpers go here. You can also create another file in app/helpers with the same format.
     # All helpers defined here will be available across all the application.
     #
